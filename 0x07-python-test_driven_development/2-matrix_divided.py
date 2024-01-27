8b1f0af8fd7df2fb150fcf51e6d4c4afe2f27ac7
nadaabdelkhalik <isawaynada5@gmail.com>
2024-01-27 09:52:45 -0800

    second

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+
+    return result_matrix
+
+    
+ 
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
