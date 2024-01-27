15e3a97883eae9ac1708ec7fbf0af69a8234949b
nadaabdelkhalik <isawaynada5@gmail.com>
2024-01-26 17:57:57 -0800

    add_0 module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 from add_0 import add
-a=1
-b=2
-print(add(a, b))
+
+if __name__ == "__main__":
+    a=1
+    b=2  
+    print("{}".format(add(a, b)))
