dcfcdfbdc7d16a22973191a0416bd22016474484
nedaabdelkhalik <isawaynada5@gmail.com>
2024-02-08 15:31:26 -0800

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base module"""
+
+class Base:
+    """Base class for managing id attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize Base instance"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
