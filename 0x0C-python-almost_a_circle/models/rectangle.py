f599b7e8719906c69f32bdabc04d7324f8f82f36
nedaabdelkhalik <isawaynada5@gmail.com>
2024-02-08 16:20:09 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,16 @@ class Rectangle(Base):
     def __str__(self):
         """Return string representation of Rectangle instance"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """Assign arguments to attributes in specified order"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
