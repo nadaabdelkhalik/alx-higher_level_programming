d8530be38f167410da1a21dedc69709791d362b4
nedaabdelkhalik <isawaynada5@gmail.com>
2024-02-08 16:09:04 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,8 +76,10 @@ class Rectangle(Base):
 
     def display(self):
         """Display the Rectangle instance with '#'"""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print('#' * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Return string representation of Rectangle instance"""
